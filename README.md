cfb8824dcb5c6efcf4c3b4220942ccd7dac4c5f3
Tim Nooteboom <tim.nooteboom@dhigroupinc.com>
2023-01-07 18:16:28 -0600

    adding code

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Advent Of Vue 2022
 
+Solution is here: https://github.com/Advent-Of-Vue/aov-sort-presents/tree/solution
+
 ## If you didn't get here from an email, read this section! Skip it otherwise.
 
 [Advent Of Vue](https://adventofvue.com) is a series of 24 Vue coding challenges that are sent out every day from December 1 to December 24 via [a dedicated newsletter](https://www.getrevue.co/profile/AdventOfVue). If you'd like to receive more of these puzzles in the future, go ahead and sign up!
--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,49 @@
       </div>
       <div class="mt-2 flex justify-center items-center">
         <img
-          v-for="present in presents"
+          v-for="present in presentsSorted"
           :key="present.id"
           :src="present.src"
           :alt="`Present ${present.id}`"
           data-qa="present"
         />
       </div>
+      <button class="bg-slate-900 hover:bg-slate-700 text-white font-semibold h-12 px-6 rounded-lg flex items-center justify-center mx-auto mt-8" @click="toggleSort">Toggle Sort</button>
     </div>
   </div>
 </template>
 
 <script setup>
-import presents from './presents.json'
+  import presents from './presents.json'
+  import { ref, computed } from 'vue'
+  let smallSort = ref(false)
+
+  const presentsSorted = computed(() => {
+    if(smallSort.value) {
+      return presents.sort((a, b) => {
+        return a.dimensions.width - b.dimensions.width
+      })
+    } else {
+      return presents.sort((a, b) => {
+        return a.id - b.id
+      })
+    }
+  })
+
+  const toggleSort = () => {
+    smallSort.value === true ? smallSort.value = false : smallSort.value = true 
+  }
+
+  // An interesting solution provided
+  /*
+  const sortFunctions = {
+    smallToBig: (a, b) => a.dimensions.width * a.dimensions.height - b.dimensions.width * b.dimensions.height,
+    default: (a, b) => a.id - b.id,
+  }
+  const sortFunctionActive = ref('default')
+  const toggleSort = () => {
+    sortFunctionActive.value = sortFunctionActive.value === 'default' ? 'smallToBig' : 'default'
+  }
+  const presentsSorted = computed(() => [...presents].sort(sortFunctions[sortFunctionActive.value]))
+  */
 </script>
